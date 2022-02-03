289d6a70c513e21e509ecf256aaad5b92b695077
ramazanyigit <54190298+ramazanyigit@users.noreply.github.com>
2022-02-03 12:13:34 +0300

    Add error checking for web application.

--- building-modern-web-applications-with-golang/02-web-application/main.go
+++ building-modern-web-applications-with-golang/02-web-application/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -32,9 +33,28 @@ func AddValues(x, y int) int {
 	return x + y
 }
 
+func Divide(w http.ResponseWriter, req *http.Request) {
+	f, err := divideValues(100.0, 0.0)
+	if err != nil {
+		fmt.Fprintln(w, "Error when dividing operation:", err)
+		return
+	}
+
+	fmt.Fprintf(w, "%f divided by %f is %f", 100.0, 10.0, f)
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y == 0 {
+		return 0, errors.New("cannot divide by 0")
+	}
+
+	return x / y, nil
+}
+
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	log.Println("Starting web application on", srvAddr)
 	err := http.ListenAndServe(srvAddr, nil)
