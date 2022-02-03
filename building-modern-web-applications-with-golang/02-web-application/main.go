df279cd02f462e0e2924995cf444d97f9940e872
ramazanyigit <54190298+ramazanyigit@users.noreply.github.com>
2022-02-03 12:04:23 +0300

    Update web application with multi-handler functions.

--- building-modern-web-applications-with-golang/02-web-application/main.go
+++ building-modern-web-applications-with-golang/02-web-application/main.go
@@ -2,19 +2,44 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
+const srvAddr = ":9000"
+
+func Home(w http.ResponseWriter, req *http.Request) {
+	numberOfBytes, err := fmt.Fprintf(w, "Hello, world!")
+
+	if err != nil {
+		fmt.Println("Cannot write response into respone writer", err)
+	} else {
+		fmt.Println("Bytes written:", numberOfBytes)
+	}
+}
+
+func About(w http.ResponseWriter, req *http.Request) {
+	numberOfBytes, err := fmt.Fprintf(w, "About page! Sum: %d", AddValues(2, 2))
+
+	if err != nil {
+		fmt.Println("Cannot write response into respone writer", err)
+	} else {
+		fmt.Println("Bytes written:", numberOfBytes)
+	}
+}
+
+func AddValues(x, y int) int {
+	return x + y
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		numberOfBytes, err := fmt.Fprintf(w, "Hello, world!")
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-		if err != nil {
-			fmt.Println("Cannot write response into respone writer", err)
-		} else {
-			fmt.Println("Bytes written:", numberOfBytes)
-		}
-	})
+	log.Println("Starting web application on", srvAddr)
+	err := http.ListenAndServe(srvAddr, nil)
 
-	http.ListenAndServe(":9000", nil)
+	if err != nil {
+		log.Panicln(err)
+	}
 }
